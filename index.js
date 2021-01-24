d1529c172f81603705c0a32609ee590079400401
otumian-empire <popecan1000@gmail.com>
2021-01-24 17:33:48 +0000

    Tenant Branch
    Add morgan
    Create tenant schema
    Create tenant api
    Fix bug: db.collection.find().toArray() is not a function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const logger = require("morgan");
 
 const adminRouter = require("./routes/admin");
 const tenantRouter = require("./routes/tenants");
@@ -10,6 +11,9 @@ const port = process.env.port || 3000;
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
+
+// Http logger
+app.use(logger("dev"));
 
 // Cors
 app.use(cors());
