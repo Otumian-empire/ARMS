5e71e9faed0f2c476eeb26c66c99f0b4109f29d8
otumian <popecan1000@gmail.com>
2021-03-31 23:47:46 +0000

    Implemented and tested the tenant route with postman

--- routes/tenants.js
+++ routes/tenants.js
@@ -3,27 +3,26 @@ require("dotenv").config();
 const tenantRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const rounds = parseInt(process.env.SALT_ROUNDS, 12);
-const tenantsDB = require("../models/tenants");
+const { Tenant } = require("../models/tenants");
 
 // fetch all tenants
 tenantRouter.get("/", (req, res) => {
-  tenantsDB.collection
-    .find()
-    .toArray()
-    .then((tenants) => res.json({ tenants }))
-    .catch((err) => {
-      console.log(err);
-      return res.json({
-        success: false,
-        message: err,
-      });
+  Tenant.find()
+    .limit(10)
+    .exec((err, tenants) => {
+      if (err)
+        return res.json({
+          success: false,
+          message: err,
+        });
+
+      return res.json({ tenants });
     });
 });
 
 // fetch a tenant
 tenantRouter.get("/:tenant_id", (req, res) => {
-  tenantsDB
-    .findOne({ _id: req.params.tenant_id })
+  Tenant.findOne({ _id: req.params.tenant_id })
     .then((tenant) => res.json({ tenant }))
     .catch((err) => {
       console.log(err);
@@ -35,7 +34,7 @@ tenantRouter.get("/:tenant_id", (req, res) => {
 });
 
 // create a tenant - add tenant data
-tenantRouter.post("/signup", (req, res) => {
+tenantRouter.post("/", (req, res) => {
   let {
     full_name,
     username,
@@ -59,20 +58,19 @@ tenantRouter.post("/signup", (req, res) => {
       });
     }
 
-    tenantsDB
-      .create({
-        full_name,
-        username,
-        password: hashPwd,
-        email,
-        phone,
-        date_of_birth,
-        address_of_previous_residence,
-        kins_full_name,
-        kins_email,
-        kins_phone,
-        kins_residence_address,
-      })
+    Tenant.create({
+      full_name,
+      username,
+      password: hashPwd,
+      email,
+      phone,
+      date_of_birth,
+      address_of_previous_residence,
+      kins_full_name,
+      kins_email,
+      kins_phone,
+      kins_residence_address,
+    })
       .then((result) => {
         if (result)
           return res.json({
@@ -95,12 +93,8 @@ tenantRouter.post("/signup", (req, res) => {
 tenantRouter.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  bcrypt.compare;
-  tenantsDB
-    .findOne({ username })
+  Tenant.findOne({ username })
     .then((result) => {
-      console.log(result);
-
       bcrypt.compare(password, result.password, (err, same) => {
         if (err)
           return res.json({
@@ -129,13 +123,13 @@ tenantRouter.post("/login", (req, res) => {
     });
 });
 
-// update - tenant may update only their emaila and phone number
+// update - tenant may update only their email and phone number
 // this applies for the next of kins
-tenantRouter.put("/update/:tenant_id", (req, res) => {
+tenantRouter.put("/:tenant_id", (req, res) => {
   const { email, phone, kins_email, kins_phone } = req.body;
   const tenant_id = req.params.tenant_id;
 
-  tenantsDB.findOne({ _id: tenant_id }, (err, tenant) => {
+  Tenant.findOne({ _id: tenant_id }, (err, tenant) => {
     if (err) {
       console.log(err);
       return res.json({ success: false, msg: err });
@@ -143,10 +137,10 @@ tenantRouter.put("/update/:tenant_id", (req, res) => {
 
     if (!tenant) return res.json({ success: false, msg: "Tenant no found" });
 
-    tenant.email = email;
-    tenant.phone = phone;
-    tenant.kins_email = kins_email;
-    tenant.kins_phone = kins_phone;
+    if (email !== undefined) tenant.email = email;
+    if (phone !== undefined) tenant.phone = phone;
+    if (kins_email !== undefined) tenant.kins_email = kins_email;
+    if (kins_phone !== undefined) tenant.kins_phone = kins_phone;
 
     tenant.save((err, updatedTenant) => {
       if (err) {
@@ -164,14 +158,17 @@ tenantRouter.put("/update/:tenant_id", (req, res) => {
 });
 
 // delete tenant data - admin privileges is needed
-tenantRouter.delete("/delete/:tenant_id", (req, res) => {
+tenantRouter.delete("/:tenant_id", (req, res) => {
   const tenant_id = req.params.tenant_id;
 
-  tenantsDB.findOneAndRemove({ _id: tenant_id }, (err, deletedTenant) => {
+  Tenant.findOneAndRemove({ _id: tenant_id }, (err, deletedTenant) => {
     if (err) {
       console.log(err);
       return res.json({ success: false, msg: err });
     }
+
+    if (!deletedTenant)
+      return res.json({ success: false, msg: "tenant no found" });
 
     return res.json({
       success: true,
