00ed613a941de1c7fa625055b0900ada3ff5c2c5
otumian <popecan1000@gmail.com>
2022-06-10 19:15:33 +0000

    Refactor src/controller/admin.controller.js - user try-catch and async await

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -8,170 +8,149 @@ import {
   INVALID_CREDENTIALS,
   LOGIN_SUCCESSFUL,
   NOT_FOUND,
-  UPDATE_SUCCESSFUL
+  UPDATE_SUCCESSFUL,
 } from "../util/api.message.js";
 import { rounds } from "../util/app.constant.js";
 
 export default class AdminController {
-  static findById(req, res) {
+  static async findById(req, res) {
     const id = req.params.id;
 
-    adminModel.findById(id)
-      .select("-password -__v")
-      .then((admin) => {
-        if (!admin) {
-          throw new Error(NOT_FOUND);
-        }
+    try {
+      const admin = await adminModel.findById(id).select("-password -__v");
 
-        return res.json(admin);
-      })
-      .catch((error) => {
-        logger.error(error);
+      if (!admin) {
+        throw new Error(NOT_FOUND);
+      }
 
-        return res.json({
-          success: false,
-          message: error.message,
-        });
+      return res.json(admin);
+    } catch (error) {
+      logger.error(error);
+
+      return res.json({
+        success: false,
+        message: error.message,
       });
+    }
   }
 
-  static create(req, res) {
-    let { username, password, email } = req.body;
+  static async create(req, res) {
+    const { username, password, email } = req.body;
 
-    hash(password, rounds, (error, hashPassword) => {
-      if (error) {
-        logger.error(error);
-
-        return res.json({
-          success: false,
-          message: AN_ERROR_OCCURRED,
-        });
-      }
+    try {
+      const hashedPassword = await hash(password, rounds);
 
       let admin = new adminModel({
         username,
         email,
-        password: hashPassword,
+        password: hashedPassword,
       });
 
-      admin.save((error, result) => {
-        if (error) {
-          logger.error(error);
+      const result = await admin.save();
 
-          return res.json({
-            success: false,
-            message: AN_ERROR_OCCURRED,
-          });
-        }
-
-        return res.json({
-          success: true,
-          message: ADMIN_CREATED_SUCCESSFULLY,
-          id: result.id,
-        });
+      return res.json({
+        success: true,
+        message: ADMIN_CREATED_SUCCESSFULLY,
+        id: result.id,
       });
-    });
+    } catch (error) {
+      logger.error(error);
+
+      return res.json({
+        success: false,
+        message: AN_ERROR_OCCURRED,
+      });
+    }
   }
 
-  static login(req, res) {
+  static async login(req, res) {
     const { username, password } = req.body;
 
-    adminModel.findOne({ username })
-      .then((result) => {
-        if (!result) {
-          throw new Error(NOT_FOUND);
-        }
+    try {
+      const result = await adminModel.findOne({ username });
 
-        compare(password, result.password, (error, same) => {
-          if (error) {
-            logger.error(error);
+      if (!result) {
+        throw new Error(NOT_FOUND);
+      }
 
-            return res.json({
-              success: false,
-              message: INVALID_CREDENTIALS,
-            });
-          }
+      const same = await compare(password, result.password);
 
-          return res.json({
-            success: true,
-            message: LOGIN_SUCCESSFUL,
-            id: result.id,
-          });
-        });
-      })
-      .catch((error) => {
-        logger.error(error);
+      if (!same) {
+        throw new Error(INVALID_CREDENTIALS)
+      }
 
-        return res.json({
-          success: false,
-          message: error.message,
-        });
+      return res.json({
+        success: true,
+        message: LOGIN_SUCCESSFUL,
+        id: result.id,
       });
+    } catch (error) {
+      logger.error(error);
+
+      return res.json({
+        success: false,
+        message: error.message,
+      });
+    }
   }
 
-  static update(req, res) {
+  static async update(req, res) {
     const id = req.params.id;
     const email = req.body.email;
 
-    adminModel.findById(id)
-      .then((result) => {
-        if (!result) {
-          throw new Error(NOT_FOUND);
-        }
+    try {
+      const result = await adminModel.findById(id);
+      if (!result) {
+        throw new Error(NOT_FOUND);
+      }
 
-        if (email) {
-          result.email = email;
-        }
+      if (email) {
+        result.email = email;
+      }
 
-        result.save((error, updatedResult) => {
-          if (error) {
-            logger.error(error);
+      const updatedResult = await result.save();
 
-            return res.json({
-              success: false,
-              message: AN_ERROR_OCCURRED,
-            });
-          }
+      if (!updatedResult) {
+        throw new Error(AN_ERROR_OCCURRED);
+      }
 
-          return res.json({
-            success: true,
-            message: UPDATE_SUCCESSFUL,
-            id: updatedResult.id,
-          });
-        });
-      })
-      .catch((error) => {
-        logger.error(error);
-
-        return res.json({
-          success: false,
-          message: error.message,
-        });
+      return res.json({
+        success: true,
+        message: UPDATE_SUCCESSFUL,
+        id: updatedResult.id,
       });
+    } catch (error) {
+      logger.error(error);
+
+      return res.json({
+        success: false,
+        message: error.message,
+      });
+    }
   }
 
-  static delete_(req, res) {
+  static async delete_(req, res) {
     const id = req.params.id;
 
-    adminModel.findByIdAndDelete(id)
-      .then((result) => {
-        if (!result) {
-          throw new Error(NOT_FOUND);
-        }
+    try {
+      const result = await adminModel.findByIdAndDelete(id);
 
-        return res.json({
-          success: true,
-          message: DELETED_SUCCESSFULLY,
-          id: result.id,
-        });
-      })
-      .catch((error) => {
-        logger.error(error);
+      if (!result) {
+        throw new Error(NOT_FOUND);
+      }
 
-        return res.json({
-          success: false,
-          message: error.message,
-        });
+      return res.json({
+        success: true,
+        message: DELETED_SUCCESSFULLY,
+        id: result.id,
       });
+    } catch (error) {
+      logger.error(error);
+
+      return res.json({
+        success: false,
+        message: error.message,
+      });
+    }
   }
 }
